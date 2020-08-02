c0262892632b409a30f83a8250f3287a2fe15126
Wasif Hossain <wasif.hossain@hotmail.com>
2020-08-02 13:28:15 +1000

    COMPLETED SECTION 3 TUTORIAL

--- src/App.js
+++ src/App.js
@@ -31,20 +31,44 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {
+          name: event.target.value,
+          age: 26,
+        },
+        {
+          name: 'Max',
+          age: 25,
+        },
+      ],
+    });
+  };
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+    };
+
     return (
       <div className='App'>
-        <h1> Wow </h1>
+        <h1> Hi, I am a React App </h1>
+        <button style={style} onClick={this.switchNameHandler}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
         />
-
-        <button onClick={this.switchNameHandler}> Switch Name</button>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
+    <div className='Person'>
       <p>
         Hi my name is {props.name} and I am {props.age} years old
       </p>
+      <input type='text' value={props.name} onChange={props.changed} />
     </div>
   );
 };
